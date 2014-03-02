972fdb064edbd24ad5b022ebdc56230b88e5c7a3
ray02202008 <ray02202008@gmail.com>
2014-03-03 00:06:33 +0800

    Create SubCalc.java

---
+++ SubCalc.java
@@ -0,0 +1,18 @@
+public class SuperCalc {
+	protected static int multiply(int a, int b){return a * b;}
+}
+
+public class SubCalc extends SuperCalc{
+	public static int multiply(int a, int b){
+		int c = super.multiply(a, b);
+//		int c = SuperCalc.multiply(a, b);
+		return c;
+	}
+	
+	public static void main(String[] args){
+		SubCalc sc = new SubCalc();
+		System.out.println(sc.multiply(3, 4));
+		System.out.println(SubCalc.multiply(2, 2));
+	}
+}
+
