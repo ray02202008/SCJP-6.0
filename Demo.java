9dd968d8b2937c5764d18f006c6822ded54152a0
ray02202008 <ray02202008@gmail.com>
2014-03-02 23:44:12 +0800

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,14 @@
+//Exam 096
+
+//import utils.*;
+//static import utils.*;
+//import utils.Repetition.*;
+//static import utils.Repetition.*;
+//import utils.Repetition.twice();
+//import static utils.Repetition.twice;
+//static import utils.Repetition.twice;
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println(twice("pizza"));
+	}
+}
